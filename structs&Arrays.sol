f8bd788a4ae12574f5bcef02039128a1f96de930
franponce <franponce98@gmail.com>
2022-10-23 03:18:09 -0300

    Dynamic array function information added 01

--- structs&Arrays.sol
+++ structs&Arrays.sol
@@ -11,3 +11,16 @@ Arreglos
 Son listas de datos, incluso se pueden hacer listas de Structs, añadiendo estructuras más complejas
 
 <type>[] <visibility> <name>;
+
+Funciones Array Dinámico
+push: Se usa para adicionar elementos al final de un array
+pop: Se usa para remover el último elemento de un array
+
+uint[] data;
+data.push(10);
+data.push(20);
+data.push(30); 
+// [10, 20, 30]
+
+data.pop(); 
+// [10, 20]
