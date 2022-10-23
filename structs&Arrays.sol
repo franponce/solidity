554518c83dc0776d19171694176264ebe015e40b
franponce <franponce98@gmail.com>
2022-10-23 03:06:37 -0300

    Structs and arrays

---
+++ structs&Arrays.sol
@@ -0,0 +1,13 @@
+Structs:
+Son estructuras de datos complejas, que agrupan una serie de datos.
+
+struct <name> {
+  <type> <name>;
+  ...
+  <type> <name>;
+}
+
+Arreglos
+Son listas de datos, incluso se pueden hacer listas de Structs, añadiendo estructuras más complejas
+
+<type>[] <visibility> <name>;
